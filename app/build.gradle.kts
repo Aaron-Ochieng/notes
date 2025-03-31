3e5a22bc56d9713b0b328e669293abbbc1515829
Aaron Ochieng <ochiengaoron@outlook.com>
2025-03-31 21:02:21 +0300

    add: kotlin room database

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("kotlin-kapt")
 }
 
 android {
@@ -57,5 +58,6 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
 
 
-
+    this.kapt(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
 }
