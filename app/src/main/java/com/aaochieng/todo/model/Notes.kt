70f209864e9920cbd866ea885502e0d25abd3efe
aaochieng <ochiengaoron@outlook.com>
2025-04-02 16:17:16 +0300

    fix: changed createdAt to string

--- app/src/main/java/com/aaochieng/todo/model/Notes.kt
+++ app/src/main/java/com/aaochieng/todo/model/Notes.kt
@@ -9,7 +9,7 @@ import java.time.LocalDateTime
 data class Note(
     val title : String,
     val content : String,
-    val createdAt : LocalDateTime,
+    val createdAt : String,
     val color : String,
 
     @PrimaryKey(autoGenerate = true)
