1953875c580bbaa745bde9d4d8bfb7ff612acd40
aaochieng <ochiengaoron@outlook.com>
2025-04-09 09:39:26 +0300

    refractor: modified title and description content

--- app/src/main/java/com/aaochieng/todo/onboading/OnBoardingModel.kt
+++ app/src/main/java/com/aaochieng/todo/onboading/OnBoardingModel.kt
@@ -12,19 +12,19 @@ sealed class OnBoardingModel(
 
     data object FirstPage: OnBoardingModel(
         image =  R.drawable.img_1,
-        title = "lorem ipsum",
-        description = "lorem ipsum"
+        title = "Stay Focused, Stay Productive",
+        description = "Break Big Goals into small steps, use our smart task manager to stay focused and accomplish more everyday."
     )
 
     data object SecondPage: OnBoardingModel(
         image =  R.drawable.img_2,
-        title = "lorem ipsum",
-        description = "lorem ipsum"
+        title = "Organize Your Day",
+        description = "Stay on Top of your tasks with a clean and simple to-do list. Plan ahead, prioritize, and get things done."
     )
 
     data object ThirdPage: OnBoardingModel(
         image =  R.drawable.img_3,
-        title = "lorem ipsum",
-        description = "lorem ipsum"
+        title = "Your Tasks, Your Way",
+        description = "Customize your workflow, set reminders, and track progress - all in one place, designed to fit your lifestyle."
     )
 }
