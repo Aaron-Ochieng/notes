c51806ad85ca76346432c9a6badf209c3ba6e154
aaochieng <ochiengaoron@outlook.com>
2025-04-09 09:40:14 +0300

    add: other params to the notes state

--- app/src/main/java/com/aaochieng/todo/model/NoteState.kt
+++ app/src/main/java/com/aaochieng/todo/model/NoteState.kt
@@ -1,7 +1,11 @@
 package com.aaochieng.todo.model
 
+
 data class NoteState(
     val notes :List<Note> = emptyList(),
     val title: String = "",
     val content : String = "",
+    val searchString : String = "",
+    val isSearchActive: Boolean = false
+
 )
